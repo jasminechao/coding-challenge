add8c2975845116588670292817ce786a4616483
Jasmine Chao <ejasminechao@gmail.com>
2019-03-31 18:00:38 +0200

    added public folder

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require "better_errors"
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
+  set :public_folder, File.expand_path('../public', __FILE__)
 end
 
 get '/' do
